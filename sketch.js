f283c951d4848063978699631ba06091306eaa70
Vidush19 <vidushsgupta2007@gmail.com>
2021-01-07 20:26:59 +0530

    changes

--- sketch.js
+++ sketch.js
@@ -9,8 +9,17 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var track, ground, car1img, car2img, car3img, car4img;
 
+function preload(){
+  track = loadImage("../images/track.jpg");
+  ground = loadImage("../images/ground.png");
+  car1img = loadImage("../images/car1.png");
+  car2img = loadImage("../images/car2.png");
+  car3img = loadImage("../images/car3.png");
+  car4img = loadImage("../images/car4.png");
 
+}
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
@@ -27,5 +36,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end();
   }
 }
